86fe341fbeb5a8670e93a675b04fa7162befcb98
tanweewen <tanweewen02@gmail.com>
2023-12-09 12:13:23 +0800

    Update

--- components/AchievementsSection.jsx
+++ components/AchievementsSection.jsx
@@ -2,37 +2,40 @@ import React from "react";
 
 const achievementsList = [
     {
+        id: 1,
         metric: "Projects",
         value: "5",
     },
     {
+        id: 2,
         metric: "Awards",
         value: "3+",
     },
     {
+        id: 3,
         metric: "Years",
         value: "1",
     }
-
 ];
+
 const AchievementsSection = () => {
-    return(
-        <div className=" xl:gap-3 sm:py-3 xl:px-12">
+    return (
+        <div className="xl:gap-3 sm:py-3 xl:px-12">
             <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
-            {achievementsList.map((achievement) => {
-                return(
-                    <div 
-                        className="flex flex-col items-center justify-center mx-4">
+                {achievementsList.map((achievement) => (
+                    <div
+                        key={achievement.id} // Add a unique key prop here
+                        className="flex flex-col items-center justify-center mx-4"
+                    >
                         <h2 className="text-white text-4xl font-bold">
                             {achievement.value}
                         </h2>
                         <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
                     </div>
-                )
-            }
-            )}
+                ))}
             </div>
-            </div>
-    )
-}
+        </div>
+    );
+};
+
 export default AchievementsSection;
