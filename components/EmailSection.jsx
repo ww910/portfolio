265546a4a43dc2db8318b7fccf5317b177096021
tanweewen <tanweewen02@gmail.com>
2023-12-09 15:14:32 +0800

    update

--- components/EmailSection.jsx
+++ components/EmailSection.jsx
@@ -6,18 +6,16 @@ import Link from "next/link";
 import Image from "next/image";
 import GmailIcon from "../public/gmail-icon.svg";
 
-const EmailSection = () => { 
-
-
+const EmailSection = () => {
     return (
         <main className="grid md:grid-cols-2 my-12 md:my-12 py-24 md:py-24 gap-4 relative" id="contact">
             <div className="z-10">
-                <h5 className="text-4xl font-bold text-white my-2 ">Let's Connect</h5>
+                <h5 className="text-4xl font-bold text-white my-2 ">Let&apos;s Connect</h5>
                 <p className="text-[#ADB7BE] mb-4 max-w-md mt-8 text-justify">
-                I&apos;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
+                    I&apos;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
                 </p>
-                <h4 className="pb-3 mt-6">Let's get in touch!</h4>
-                <div className=" flex flex-row gap-4 mt-6">
+                <h4 className="pb-3 mt-6">Let&apos;s get in touch!</h4>
+                <div className="flex flex-row gap-4 mt-6">
                     <Link href="https://github.com/ww910">
                         <Image src={GithubIcon} alt="Github Icon" />
                     </Link>
@@ -25,12 +23,11 @@ const EmailSection = () => {
                         <Image src={LinkedinIcon} alt="Linkedin Icon" />
                     </Link>
                     <Link href="https://www.instagram.com/tanweewen/" className="mt-1">
-                        <Image src={InstagramIcon}/>
+                        <Image src={InstagramIcon} alt="Instagram Icon" />
                     </Link>
                     <Link href="mailto:tanweewen02@gmail.com" className="mt-1">
-                        <Image src={GmailIcon}/>
+                        <Image src={GmailIcon} alt="Gmail Icon" />
                     </Link>
-
                 </div>
             </div>
         </main>
